b6651a16ce917febc136bc03ac4117fa32bfdc8d
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2026-01-13 23:54:02 +0900

    Add buckets to constructor and bucket, entry, set and get methods

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,5 +1,60 @@
 export class HashMap {
     constructor() {
-        
+        this.loadFactor = 0.75;
+        this.capacity = 16;
+        this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNum = 31;
+        for(let i = 0; i < key.length; i ++) {
+            hashCode = primeNum * hashCode + key.charCodeAt(i) % this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    bucket(key) {
+        let hash = this.hash(key);
+        return this.buckets[hash % this.buckets.length];
+    }
+
+    entry(bucket, key) {
+        for(let entry of bucket) {
+            if(entry.key === key) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    set(key, value) {
+        let bucket = this.bucket(key);
+        let entry = this.entry(bucket, key);
+        if(entry) {
+            entry.value = value;
+            return;
+        }
+        bucket.push({ key, value });
+    }
+
+    get(key) {
+        let bucket = this.bucket(key);
+        let entry = this.entry(bucket, key);
+        if(entry) {
+            return entry.value;
+        }
+        return null;
     }
 }
+
+/*
+Use the following snippet whenever you access a bucket through an index
+
+if (index < 0 || index >= buckets.length) {
+  throw new Error("Trying to access index out of bounds");
+}
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import { HashMap } from "./hashmap";
 
 const test = new HashMap();
+
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+test.set('lion', 'test');
+
+console.log(test.buckets);
+console.log(test.get('frog'));
